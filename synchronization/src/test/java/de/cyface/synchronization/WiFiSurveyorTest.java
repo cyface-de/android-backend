bda3b5ec00428d8f780ae5f75aae5276fa96b971
Klemens Muthmann <klemens_muthmann@gmx.de>
2018-03-29 17:15:37 +0200

    Add WiFiSurveyorTest for non WiFi State
    This test adds the WiFiSurveyorTest for the state were only mobile is
    connected or no connection exists. To debug that test even with the
    current bug in Robolectric it also adds additional output when running
    tests directly via Gradle.

--- synchronization/src/test/java/de/cyface/synchronization/WiFiSurveyorTest.java
+++ synchronization/src/test/java/de/cyface/synchronization/WiFiSurveyorTest.java
@@ -1,10 +1,25 @@
 package de.cyface.synchronization;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowConnectivityManager;
+import org.robolectric.shadows.ShadowNetworkInfo;
+
+import android.accounts.Account;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 
 /**
  * Tests the correct functionality of the <code>WiFiSurveyor</code> class. This test requires an active WiFi connection
@@ -20,69 +35,73 @@ import org.robolectric.annotation.Config;
 @Config(constants = BuildConfig.class)
 public class WiFiSurveyorTest {
 
-    /*
-     * private Context context;
-     * private ShadowConnectivityManager shadowConnectivityManager;
-     * private WiFiSurveyor oocut;
-     */
+    private Context context;
+    private ShadowConnectivityManager shadowConnectivityManager;
+    private WiFiSurveyor oocut;
 
     @Before
     public void setUp() {
-        // context = RuntimeEnvironment.application;
-        // ConnectivityManager connectivityManager = getConnectivityManager();
-        // shadowConnectivityManager = Shadows.shadowOf(connectivityManager);
-        // oocut = new WiFiSurveyor(context, getConnectivityManager());
+        context = RuntimeEnvironment.application;
+        ConnectivityManager connectivityManager = getConnectivityManager();
+        shadowConnectivityManager = Shadows.shadowOf(connectivityManager);
+        oocut = new WiFiSurveyor(context, connectivityManager);
     }
 
     @Test
     public void testWifiConnectivity() throws InterruptedException, SynchronisationException {
-        /*
-         * switchWiFiConnection(false);
-         * assertThat(oocut.isConnected(), is(equalTo(false)));
-         * Account account = oocut.getOrCreateAccount("test");
-         * oocut.startSurveillance(account);
-         * switchWiFiConnection(true);
-         * assertThat(oocut.isConnected(), is(equalTo(true)));
-         * assertThat(oocut.synchronizationIsActive(), is(equalTo(true)));
-         */
+        switchWiFiConnection(false);
+        assertThat(oocut.isConnected(), is(equalTo(false)));
+        Account account = oocut.getOrCreateAccount("test");
+        oocut.startSurveillance(account);
+        switchWiFiConnection(true);
+        assertThat(oocut.isConnected(), is(equalTo(true)));
+        assertThat(oocut.synchronizationIsActive(), is(equalTo(true)));
     }
 
     @Test
     public void testMobileConnectivity() throws SynchronisationException {
-        // switchMobileConnection(false);
-        /*
-         * switchWiFiConnection(false);
-         * oocut.syncOnWiFiOnly(false);
-         * assertThat(oocut.isConnected(), is(equalTo(false)));
-         * switchMobileConnection(true);
-         * assertThat(oocut.isConnected(), is(equalTo(true)));
-         */
+        switchMobileConnection(false);
 
+        switchWiFiConnection(false);
+        oocut.syncOnWiFiOnly(false);
+        assertThat(oocut.isConnected(), is(equalTo(false)));
+        switchMobileConnection(true);
+        assertThat(oocut.isConnected(), is(equalTo(true)));
     }
-    /*
-     * private ConnectivityManager getConnectivityManager() {
-     * return (ConnectivityManager)RuntimeEnvironment.application.getSystemService(context.CONNECTIVITY_SERVICE);
-     * }
-     * private void switchWiFiConnection(final boolean enabled) {
-     * NetworkInfo networkInfoShadow = ShadowNetworkInfo.newInstance(NetworkInfo.DetailedState.CONNECTED,
-     * ConnectivityManager.TYPE_WIFI, 0, true, enabled);
-     * shadowConnectivityManager.setNetworkInfo(ConnectivityManager.TYPE_WIFI, networkInfoShadow);
-     * if (enabled) {
-     * shadowConnectivityManager.setActiveNetworkInfo(networkInfoShadow);
-     * }
-     * Intent broadcastIntent = new Intent(ConnectivityManager.CONNECTIVITY_ACTION);
-     * broadcastIntent.putExtra(WifiManager.EXTRA_SUPPLICANT_CONNECTED, enabled);
-     * RuntimeEnvironment.application.sendBroadcast(broadcastIntent);
-     * }
-     * private void switchMobileConnection(final boolean enabled) {
-     * NetworkInfo networkInfoShadow = ShadowNetworkInfo.newInstance(NetworkInfo.DetailedState.CONNECTED,
-     * ConnectivityManager.TYPE_MOBILE, 0, true, enabled);
-     * shadowConnectivityManager.setNetworkInfo(ConnectivityManager.TYPE_MOBILE, networkInfoShadow);
-     * if (enabled) {
-     * shadowConnectivityManager.setActiveNetworkInfo(networkInfoShadow);
-     * }
-     * Intent broadcastIntent = new Intent(ConnectivityManager.CONNECTIVITY_ACTION);
-     * RuntimeEnvironment.application.sendBroadcast(broadcastIntent);
-     * }
-     */
+
+    private ConnectivityManager getConnectivityManager() {
+        return (ConnectivityManager)RuntimeEnvironment.application.getSystemService(context.CONNECTIVITY_SERVICE);
+    }
+
+    private void switchWiFiConnection(final boolean enabled) {
+        NetworkInfo networkInfoShadow = ShadowNetworkInfo.newInstance(
+                enabled ? NetworkInfo.DetailedState.CONNECTED : NetworkInfo.DetailedState.DISCONNECTED,
+                ConnectivityManager.TYPE_WIFI, 0, true,
+                enabled ? NetworkInfo.State.CONNECTED : NetworkInfo.State.DISCONNECTED);
+        shadowConnectivityManager.setNetworkInfo(ConnectivityManager.TYPE_WIFI, networkInfoShadow);
+        if (enabled) {
+            shadowConnectivityManager.setActiveNetworkInfo(networkInfoShadow);
+        } else {
+            shadowConnectivityManager.setActiveNetworkInfo(null);
+        }
+        Intent broadcastIntent = new Intent(ConnectivityManager.CONNECTIVITY_ACTION);
+        broadcastIntent.putExtra(WifiManager.EXTRA_SUPPLICANT_CONNECTED, enabled);
+        RuntimeEnvironment.application.sendBroadcast(broadcastIntent);
+    }
+
+    private void switchMobileConnection(final boolean enabled) {
+        NetworkInfo networkInfoShadow = ShadowNetworkInfo.newInstance(
+                enabled ? NetworkInfo.DetailedState.CONNECTED : NetworkInfo.DetailedState.DISCONNECTED,
+                ConnectivityManager.TYPE_MOBILE, 0, true,
+                enabled ? NetworkInfo.State.CONNECTED : NetworkInfo.State.DISCONNECTED);
+        shadowConnectivityManager.setNetworkInfo(ConnectivityManager.TYPE_MOBILE, networkInfoShadow);
+        if (enabled) {
+            shadowConnectivityManager.setActiveNetworkInfo(networkInfoShadow);
+        } else {
+            shadowConnectivityManager.setActiveNetworkInfo(null);
+        }
+        Intent broadcastIntent = new Intent(ConnectivityManager.CONNECTIVITY_ACTION);
+        RuntimeEnvironment.application.sendBroadcast(broadcastIntent);
+    }
+
 }
