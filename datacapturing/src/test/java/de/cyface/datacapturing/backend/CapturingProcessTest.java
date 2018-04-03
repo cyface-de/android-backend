18147e0a7328ea6d5de07843f761ed812ba810a7
Klemens Muthmann <klemens_muthmann@gmx.de>
2018-04-03 22:17:39 +0200

    Create empty/failing robolectic test for capture

---
+++ datacapturing/src/test/java/de/cyface/datacapturing/backend/CapturingProcessTest.java
@@ -0,0 +1,22 @@
+package de.cyface.datacapturing.backend;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class CapturingProcessTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testCaptureSensorDataAlongWithGeoLocation() {
+        fail("Not yet implemented!");
+    }
+}
