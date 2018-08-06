43d332390f0c118655584295c08604f8945e7928
Klemens Muthmann <klemens_muthmann@gmx.de>
2018-08-06 19:59:25 +0200

    Add missing documentation to the new test case

--- datacapturing/src/androidTest/java/de/cyface/datacapturing/persistence/MeasurementPersistenceTest.java
+++ datacapturing/src/androidTest/java/de/cyface/datacapturing/persistence/MeasurementPersistenceTest.java
@@ -18,12 +18,25 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Tests the correct workings of the <code>MeasurementPersistence</code> class.
+ *
+ * @author Klemens Muthmann
+ * @version 1.0.0
+ * @since 2.0.3
+ */
 @RunWith(AndroidJUnit4.class)
 @MediumTest
 public class MeasurementPersistenceTest {
 
+    /**
+     * An object of the class under test. It is setup prior to each test execution.
+     */
     private MeasurementPersistence oocut;
 
+    /**
+     * Initializes the <code>oocut</code> with the Android persistence stack.
+     */
     @Before
     public void setUp() {
         Context context = InstrumentationRegistry.getTargetContext();
@@ -31,12 +44,21 @@ public class MeasurementPersistenceTest {
         oocut = new MeasurementPersistence(resolver);
     }
 
+    /**
+     * Deletes all content from the content provider, to leave the next test with a clean test environment.
+     */
     @After
     public void tearDown() {
         oocut.clear();
         oocut.shutdown();
     }
 
+    /**
+     * Inserts two measurements into the database; one finished and one still running and checks, that the
+     * <code>loadFinishedMeasurements</code> method returns a list of size 1.
+     *
+     * @throws DataCapturingException Fails the test if anything unexpected happens.
+     */
     @Test
     public void testLoadFinishedMeasurements_oneFinishedOneRunning() throws DataCapturingException {
         oocut.newMeasurement(Vehicle.UNKOWN);
@@ -45,11 +67,14 @@ public class MeasurementPersistenceTest {
         assertThat(oocut.hasOpenMeasurement(), is(equalTo(false)));
         oocut.newMeasurement(Vehicle.UNKOWN);
         assertThat(oocut.hasOpenMeasurement(), is(equalTo(true)));
-        assertThat(oocut.loadFinishedMeasurements().size(),is(equalTo(1)));
+        assertThat(oocut.loadFinishedMeasurements().size(), is(equalTo(1)));
     }
 
+    /**
+     * Checks that calling <code>loadFinishedMeasurements</code> on an empty database returns an empty list.
+     */
     @Test
     public void testLoadFinishedMeasurements_noMeasurements() {
-        assertThat(oocut.loadFinishedMeasurements().isEmpty(),is(equalTo(true)));
+        assertThat(oocut.loadFinishedMeasurements().isEmpty(), is(equalTo(true)));
     }
 }
