118e68ec9c4e4e4e4e1b88e8b45b95fd0bec169f
Klemens Muthmann <klemens_muthmann@gmx.de>
2018-08-13 14:01:03 +0200

    Add missing documentation

--- synchronization/src/test/java/de/cyface/synchronization/TestUtils.java
+++ synchronization/src/test/java/de/cyface/synchronization/TestUtils.java
@@ -1,5 +1,22 @@
 package de.cyface.synchronization;
 
+/**
+ * Contains constants and utility methods required during testing.
+ *
+ * @author Klemens Muthmann
+ * @version 1.0.0
+ * @since 2.1.0
+ */
 final class TestUtils {
+    /**
+     * The content provider authority used for testing.
+     */
     public final static String AUTHORITY = "de.cyface.synchronization.provider.test";
+
+    /**
+     * Private constructor to avoid instantiation of utility class.
+     */
+    private TestUtils() {
+        // Nothing to do here.
+    }
 }
