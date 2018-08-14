c5c08bc811795715268c4526a6c240e74bc6730a
Armin <armin.schnabel@gmail.com>
2018-08-14 10:47:34 +0200

    Adjusts the authenticator for #CY-3816

--- synchronization/src/cyface/java/de/cyface/synchronization/Constants.java
+++ synchronization/src/cyface/java/de/cyface/synchronization/Constants.java
@@ -1,5 +1,13 @@
 package de.cyface.synchronization;
 
+/**
+ * Final static constants used by multiple classes.
+ *
+ * @author Klemens Muthmann
+ * @author Armin Schnabel
+ * @version 1.0.0
+ * @since 2.0.0
+ */
 public final class Constants {
 
     final static int GEO_LOCATIONS_UPLOAD_BATCH_SIZE = 10;
@@ -9,6 +17,8 @@ public final class Constants {
 
     public final static String ACCOUNT_TYPE = "de.cyface.account";
     public final static String AUTH_TOKEN_TYPE = "de.cyface.auth_token_type";
+    //FIXME: What is this for / is this necessary?
+    public final static String ARG_IS_ADDING_NEW_ACCOUNT = "de.cyface.arg_is_adding_new_account";
 
     public final static String DEFAULT_FREE_USERNAME = "playStoreBeta";
     public final static String DEFAULT_FREE_PASSWORD = "playStoreBeta@Cy";
