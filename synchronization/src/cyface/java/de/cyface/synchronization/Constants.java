c07886cc9d401316488c2b51a7ec6924b3e4edf7
Armin <armin.schnabel@gmail.com>
2018-08-23 16:21:37 +0200

    Removes deprecated constants #CY-3974

--- synchronization/src/cyface/java/de/cyface/synchronization/Constants.java
+++ synchronization/src/cyface/java/de/cyface/synchronization/Constants.java
@@ -5,7 +5,7 @@ package de.cyface.synchronization;
  *
  * @author Klemens Muthmann
  * @author Armin Schnabel
- * @version 1.1.0
+ * @version 1.2.0
  * @since 2.0.0
  */
 public final class Constants {
@@ -17,23 +17,6 @@ public final class Constants {
 
     // This may be used by all implementing apps, thus, public
     public final static String AUTH_TOKEN_TYPE = "de.cyface.auth_token_type";
-
-    // FIXME: Duplicate constants - move ErrorHandler to SDK
-    static final String ERROR_INTENT = "de.cyface.error";
-    static final String ERROR_CODE_EXTRA = "de.cyface.error.error_code";
-    final static String HTTP_CODE_EXTRA = "de.cyface.error.http_code";
-    public static final int UNKNOWN_EC = 0;
-    public static final int UNAUTHORIZED_EC = 1;
-    public static final int MALFORMED_URL_EC = 2;
-    public static final int HTTP_RESPONSE_UNREADABLE_EC = 3;
-    public static final int SERVER_UNAVAILABLE_EC = 4;
-    public static final int NETWORK_ERROR_EC = 5;
-    public static final int DATABASE_ERROR_EC = 6;
-    public static final int AUTHENTICATION_ERROR_EC = 7;
-    public static final int AUTHENTICATION_CANCELED_ERROR_EC = 8;
-    public static final int SYNCHRONIZATION_ERROR_EC = 9;
-    public static final int DATA_TRANSMISSION_ERROR_EC = 10;
-    // public static final int MEASUREMENT_ENTRY_IS_IRRETRIEVABLE = X;
 
     private Constants() {
         // Nothing to do here.
