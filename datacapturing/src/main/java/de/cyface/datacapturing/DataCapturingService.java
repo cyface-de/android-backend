f56bbd3c35c8f4677dd2e706f19f9f3829df773b
Klemens Muthmann <klemens.muthmann@gmail.com>
2018-09-04 12:43:45 +0200

    Increase version of DataCapturingService

--- datacapturing/src/main/java/de/cyface/datacapturing/DataCapturingService.java
+++ datacapturing/src/main/java/de/cyface/datacapturing/DataCapturingService.java
@@ -65,7 +65,7 @@ import de.cyface.synchronization.WiFiSurveyor;
  * <code>Activity</code> lifecycle.
  *
  * @author Klemens Muthmann
- * @version 7.0.0
+ * @version 7.0.1
  * @since 1.0.0
  */
 public abstract class DataCapturingService {
