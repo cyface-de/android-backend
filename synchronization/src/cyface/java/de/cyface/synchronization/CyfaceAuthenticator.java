213fb606269d3750ad5b7de3f526e3fa7fbf0c94
Armin <armin.schnabel@gmail.com>
2018-09-04 15:49:41 +0200

    Fixes wrong error code for invalid credentials on Android 4.1.2 #CY-4035

--- synchronization/src/cyface/java/de/cyface/synchronization/CyfaceAuthenticator.java
+++ synchronization/src/cyface/java/de/cyface/synchronization/CyfaceAuthenticator.java
@@ -115,6 +115,7 @@ public final class CyfaceAuthenticator extends AbstractAccountAuthenticator {
                     sendErrorIntent(context, DATA_TRANSMISSION_ERROR.getCode(), e.getHttpStatusCode());
                     throw new NetworkErrorException(e);
                 } catch (final ResponseParsingException e) {
+                    Log.d(TAG, String.format("GetAuthToken failed: Error %s.", e.getMessage()));
                     sendErrorIntent(context, UNREADABLE_HTTP_RESPONSE.getCode());
                     throw new NetworkErrorException(e);
                 } catch (final UnauthorizedException e) {
