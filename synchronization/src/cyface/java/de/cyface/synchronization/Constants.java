9c8b65308a135e63182b7860d2d8fe08bd8c7023
Armin <armin.schnabel@gmail.com>
2018-09-05 12:38:34 +0200

    Increases geoloaction upload batch size from 10 to 1_500
    1_500 as the total batch size of 7_500 worked good before and 2_000 is the current batch sizes for each of the three point types.

--- synchronization/src/cyface/java/de/cyface/synchronization/Constants.java
+++ synchronization/src/cyface/java/de/cyface/synchronization/Constants.java
@@ -5,12 +5,19 @@ package de.cyface.synchronization;
  *
  * @author Klemens Muthmann
  * @author Armin Schnabel
- * @version 1.2.0
+ * @version 1.2.1
  * @since 2.0.0
  */
 public final class Constants {
 
-    final static int GEO_LOCATIONS_UPLOAD_BATCH_SIZE = 10;
+    /**
+     * Increasing the _total batch size_ (i.e. sum of all data type batch sizes) leads to more data
+     * loaded into the memory.
+     *
+     * Increasing _on of the batch sizes_ can lead to a "FAILED BINDER TRANSACTION". *
+     * In #CY-3859 this occurred with a parcel size of ~1_380_000 (batch size 6_000).
+     */
+    final static int GEO_LOCATIONS_UPLOAD_BATCH_SIZE = 1_500;
     final static int ACCELERATIONS_UPLOAD_BATCH_SIZE = 2_000;
     final static int ROTATIONS_UPLOAD_BATCH_SIZE = 2_000;
     final static int DIRECTIONS_UPLOAD_BATCH_SIZE = 2_000;
