1ed7f08010847bea22e73b0c9bccc15d16b527d7
Klemens Muthmann <klemens_muthmann@gmx.de>
2018-09-13 14:43:54 +0200

    Fix missing dependency

--- utils/src/main/java/de/cyface/utils/ErrorHandler.java
+++ utils/src/main/java/de/cyface/utils/ErrorHandler.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
 
 /**
  * Maintains and informs {@link ErrorListener}. This class is responsible for Cyface Errors.
