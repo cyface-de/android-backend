cde4c178daef715edd42d86175f9748de767a295
Armin <armin.schnabel@gmail.com>
2018-09-14 13:12:21 +0200

    Implement the PR review feedback CY-4080

--- datacapturing/src/cyface/java/de/cyface/datacapturing/CyfaceDataCapturingService.java
+++ datacapturing/src/cyface/java/de/cyface/datacapturing/CyfaceDataCapturingService.java
@@ -34,17 +34,36 @@ public final class CyfaceDataCapturingService extends DataCapturingService {
      * @param dataUploadServerAddress The server address running an API that is capable of receiving data captured by
      *            this service.
      * @param eventHandlingStrategy The {@link EventHandlingStrategy} used to react to selected events
-     *                              triggered by the {@link DataCapturingBackgroundService}.
+     *            triggered by the {@link DataCapturingBackgroundService}.
      * @throws SetupException If writing the components preferences or registering the dummy user account fails.
      */
     public CyfaceDataCapturingService(final @NonNull Context context, final @NonNull ContentResolver contentResolver,
             final @NonNull String authority, final @NonNull String accountType,
-            final @NonNull String dataUploadServerAddress, final @Nullable EventHandlingStrategy eventHandlingStrategy)
+            final @NonNull String dataUploadServerAddress, final @NonNull EventHandlingStrategy eventHandlingStrategy)
             throws SetupException {
         super(context, contentResolver, authority, accountType, dataUploadServerAddress, eventHandlingStrategy);
         if (LOGIN_ACTIVITY == null) {
             throw new IllegalStateException("No LOGIN_ACTIVITY was set from the SDK using app.");
         }
+    }
+
+    /**
+     * Creates a new completely initialized {@link DataCapturingService}.
+     *
+     * @param context The context (i.e. <code>Activity</code>) handling this service.
+     * @param contentResolver Resolver used to access the content provider for storing measurements.
+     * @param authority The <code>ContentProvider</code> authority used to identify the content provider used by this
+     *            <code>DataCapturingService</code>. You should use something world wide unqiue, like your domain, to
+     *            avoid collisions between different apps using the Cyface SDK.
+     * @param accountType The type of the account to use to synchronize data.
+     * @param dataUploadServerAddress The server address running an API that is capable of receiving data captured by
+     *            this service.
+     * @throws SetupException If writing the components preferences or registering the dummy user account fails.
+     */
+    public CyfaceDataCapturingService(final @NonNull Context context, final @NonNull ContentResolver contentResolver,
+            final @NonNull String authority, final @NonNull String accountType,
+            final @NonNull String dataUploadServerAddress) throws SetupException {
+        this(context, contentResolver, authority, accountType, dataUploadServerAddress, new IgnoreEventsStrategy());
     }
 
     /**
