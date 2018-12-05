edc598527ffecdd2c67c341ddb0df7d13f3d3f8d
Armin <armin.schnabel@gmail.com>
2018-12-05 11:19:33 +0100

    Clean up CapturedDataWriter

--- datacapturing/src/main/java/de/cyface/datacapturing/persistence/CapturedDataWriter.java
+++ datacapturing/src/main/java/de/cyface/datacapturing/persistence/CapturedDataWriter.java
@@ -7,6 +7,8 @@ import android.util.Log;
 
 import de.cyface.datacapturing.model.CapturedData;
 import de.cyface.persistence.serialization.AccelerationsFile;
+import de.cyface.persistence.serialization.DirectionsFile;
+import de.cyface.persistence.serialization.RotationsFile;
 
 /**
  * A class responsible for writing captured sensor data to the underlying persistence layer.
@@ -29,7 +31,18 @@ public class CapturedDataWriter implements Runnable {
      * The data to write.
      */
     private final CapturedData data;
+    /**
+     * The file to write acceleration points to.
+     */
     private final AccelerationsFile accelerationsFile;
+    /**
+     * The file to write rotation points to.
+     */
+    private final RotationsFile rotationsFile;
+    /**
+     * The file to write direction points to.
+     */
+    private final DirectionsFile directionsFile;
     /**
      * Callback which is called after writing data has finished.
      */
@@ -43,9 +56,12 @@ public class CapturedDataWriter implements Runnable {
      * @param callback Callback which is called after writing data has finished.
      */
     CapturedDataWriter(final @NonNull CapturedData data, final AccelerationsFile accelerationsFile,
+            final RotationsFile rotationsFile, final DirectionsFile directionsFile,
             final @NonNull WritingDataCompletedCallback callback) {
         this.data = data;
         this.accelerationsFile = accelerationsFile;
+        this.rotationsFile = rotationsFile;
+        this.directionsFile = directionsFile;
         this.callback = callback;
     }
 
@@ -54,6 +70,8 @@ public class CapturedDataWriter implements Runnable {
                 + data.getDirections().size() + " A/R/MPs on: " + Thread.currentThread().getName());
 
         accelerationsFile.append(data.getAccelerations());
+        rotationsFile.append(data.getRotations());
+        directionsFile.append(data.getDirections());
     }
 
     @Override
