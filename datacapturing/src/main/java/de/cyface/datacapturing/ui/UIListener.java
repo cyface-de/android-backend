d9595f89afcae8210165a69728a3acccca0ddd35
Armin <armin.schnabel@gmail.com>
2019-02-06 17:23:05 +0100

    Make api methods used by SR available again and document usages #MOV-530

--- datacapturing/src/main/java/de/cyface/datacapturing/ui/UIListener.java
+++ datacapturing/src/main/java/de/cyface/datacapturing/ui/UIListener.java
@@ -12,6 +12,7 @@ import de.cyface.datacapturing.DataCapturingService;
  * @version 1.0.1
  * @since 2.0.0
  */
+// Needs to be public as this interface is implemented by sdk implementing apps (SR)
 public interface UIListener {
     /**
      * Handler for location changes occurring even while no tracking is active.
