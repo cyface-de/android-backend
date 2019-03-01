549dfa995bb30636f468b49a6558238e4196c6ca
Armin <armin.schnabel@gmail.com>
2019-03-01 09:22:42 +0100

    Bump code versions

--- persistence/src/main/java/de/cyface/persistence/PersistenceLayer.java
+++ persistence/src/main/java/de/cyface/persistence/PersistenceLayer.java
@@ -45,7 +45,7 @@ import de.cyface.utils.Validate;
  *
  * @author Klemens Muthmann
  * @author Armin Schnabel
- * @version 11.0.0
+ * @version 11.0.1
  * @since 2.0.0
  */
 public class PersistenceLayer<B extends PersistenceBehaviour> {
--- synchronization/src/main/java/de/cyface/synchronization/WiFiSurveyor.java
+++ synchronization/src/main/java/de/cyface/synchronization/WiFiSurveyor.java
@@ -31,7 +31,7 @@ import de.cyface.utils.Validate;
  *
  * @author Klemens Muthmann
  * @author Armin Schnabel
- * @version 5.0.0
+ * @version 5.0.1
  * @since 2.0.0
  */
 public class WiFiSurveyor extends BroadcastReceiver {
