5769cade055e046ce935a566b105f6ca17baface
Armin <armin.schnabel@gmail.com>
2019-04-02 10:29:00 +0200

    Add time unit as requested in the PR review #131

--- datacapturing/src/androidTest/java/de/cyface/datacapturing/TestUtils.java
+++ datacapturing/src/androidTest/java/de/cyface/datacapturing/TestUtils.java
@@ -45,7 +45,7 @@ public class TestUtils {
     static final String DEFAULT_USERNAME = "admin";
     static final String DEFAULT_PASSWORD = "secret";
     /**
-     * The time to wait for the pong message to return and for the service to start or stop.
+     * The amount of seconds to wait for the pong message to return and for the service to start or stop.
      */
     public static final long TIMEOUT_TIME = 10L;
 
