523d37a34259a3c1cb065985b3652978e86bbe0e
Armin <armin.schnabel@cyface.de>
2024-03-14 09:40:57 +0100

    Upgrade dependencies (+ sync dependency versions w/main)

--- datacapturing/src/test/java/de/cyface/datacapturing/backend/DataCapturingTest.java
+++ datacapturing/src/test/java/de/cyface/datacapturing/backend/DataCapturingTest.java
@@ -18,7 +18,7 @@
  */
 package de.cyface.datacapturing.backend;
 
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
