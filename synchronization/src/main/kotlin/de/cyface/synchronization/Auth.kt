bc005acc728e63f83ca41807156d0489ca7d5e17
Armin <armin.schnabel@cyface.de>
2024-05-30 11:52:09 +0200

    [RFR-1046] Add logout and userId to Auth interface

--- synchronization/src/main/kotlin/de/cyface/synchronization/Auth.kt
+++ synchronization/src/main/kotlin/de/cyface/synchronization/Auth.kt
@@ -1,5 +1,31 @@
+/*
+ * Copyright 2023-2024 Cyface GmbH
+ *
+ * This file is part of the Cyface SDK for Android.
+ *
+ * The Cyface SDK for Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Cyface SDK for Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Cyface SDK for Android. If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.cyface.synchronization
 
+import androidx.fragment.app.FragmentActivity
+import net.openid.appauth.IdToken
+
+/**
+ * The interface to authenticate the user and get the user id.
+ *
+ * @author Armin Schnabel
+ */
 interface Auth {
 
     /**
@@ -14,4 +40,14 @@ interface Auth {
             ex: Exception?
         ) -> Unit
     )
+
+    /**
+     * @return The `userId` or `null` if not available.
+     */
+    fun userId(): String?
+
+    /**
+     * Sends the end session request to the auth service to sign out the user.
+     */
+    fun endSession(activity: FragmentActivity)
 }
